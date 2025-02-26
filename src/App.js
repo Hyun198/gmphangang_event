77311b5472021f9e0cc577f6858e4436db0046bd
Hyun <hwn123h@naver.com>
2025-02-26 11:03:53 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const imgs = [
+    "/imgs/당근 홍보용 자료01.jpg",
+    "/imgs/당근 홍보용 자료02.jpg",
+    "/imgs/당근 홍보용 자료03.jpg",
+    "/imgs/당근 홍보용 자료04.jpg",
+    "/imgs/당근 홍보용 자료05.jpg",
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+
+      <main>
+        {imgs.map((src, index) => (
+          <img className="imgs" key={index} src={src} alt={`당근홍보_${index + 1}`} />
+        ))}
+      </main>
+
+
+      <footer>
+
+      </footer>
     </div>
   );
 }
